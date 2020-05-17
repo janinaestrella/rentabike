9539cabb00b72a80d6000d6d21502c1f8cda42c5
Janina Estrella <janina.estrella@gmail.com>
2020-05-17 20:06:05 +0800

    database and filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    // 'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'public'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,11 +51,12 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            
+            // 'root' => storage_path('app/public'),
+            // 'url' => env('APP_URL').'/storage',
 
-            // 'root' => 'public',
-            // 'url' => env('APP_URL').'/public',
+            'root' => 'public',
+            'url' => env('APP_URL').'/public',
 
             'visibility' => 'public',
         ],
