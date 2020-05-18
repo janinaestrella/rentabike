7dfad1af709b31fb5323ccd63427ad5ca969ee08
Janina Estrella <janina.estrella@gmail.com>
2020-05-18 14:20:59 +0800

    navbar links are not viewable by guests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,12 +26,14 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                @auth
                     <ul class="navbar-nav mr-auto">
                     @include("partials.navlink", [
                             'url' => route('categories.index'),
@@ -48,13 +50,13 @@
                             'displayName' => 'My Requests'
                             ])
                     </ul>
-
+                @endauth
                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                     
+                    @auth
                          <li class="nav-item">
                             <a href="{{route('bikerequests.index')}}" class="nav-link">Bike Request Form
                                 <span id="cart-count" class="badge badge-primary">
@@ -63,7 +65,7 @@
 
                             </a>
                         </li>
-
+                    @endauth
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
